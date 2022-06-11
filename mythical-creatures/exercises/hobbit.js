db04eb4cf4637856c1ade5910134191f6b2792e8
Katie Marcks <katiemarcks7@gmail.com>
2022-06-11 06:51:58 -0600

    Pass 7 of 8 hobbit tests

---
+++ mythical-creatures/exercises/hobbit.js
@@ -0,0 +1,19 @@
+class Hobbit {
+  constructor({ name }) {
+    this.name = name;
+    this.age = 0;
+    this.adult = false;
+    this.old = false;
+  }
+
+  celebrateBirthday() {
+    this.age++;
+    if (this.age > 32 && this.age < 101) {
+      this.adult = true;
+    } else if (this.age >= 101) {
+      this.old = true;
+    }
+  }
+}
+
+module.exports = Hobbit;

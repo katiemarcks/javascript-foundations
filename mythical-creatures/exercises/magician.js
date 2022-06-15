bfcbf018a34e8a8e21ed75a3d5a4e0e68297c41f
Katie Marcks <katiemarcks7@gmail.com>
2022-06-15 09:33:26 -0600

    Pass all magician tests

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -3,10 +3,28 @@ class Magician {
     this.name = `The Great ${name}`;
     this.assistant = assistant;
     this.favoriteAccessory = clothing || "top hat";
+    this.confidencePercentage = 10;
   }
 
   performIncantation(string) {
     return `${string.toUpperCase()}!`;
+  }
+
+  performTrick() {
+    this.confidencePercentage = this.confidencePercentage + 10;
+    if (this.favoriteAccessory === "top hat") {
+      return "PULL RABBIT FROM TOP HAT";
+    } else {
+      return "PULL DOVE FROM SLEEVE";
+    }
+  }
+
+  performShowStopper() {
+    if (this.assistant && this.confidencePercentage >= 100) {
+      return "WOW! The magician totally just sawed that person in half!";
+    } else {
+      return "Oh no, this trick is not ready!";
+    }
   }
 }
 

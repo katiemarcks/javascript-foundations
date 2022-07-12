5963e70f3af8b9296857f89262da8ac3c65b9020
Katie Marcks <katiemarcks7@gmail.com>
2022-07-12 07:33:03 -0600

    Pass all sphinx tests

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -1,6 +1,7 @@
 class Sphinx {
   constructor(name = null) {
     this.riddles = [];
+    this.heroesEaten = 0;
   }
 
   collectRiddle(riddle) {
@@ -16,7 +17,15 @@ class Sphinx {
       if (element.answer === answer) {
         let index = this.riddles.indexOf(element);
         this.riddles.splice(index, 1);
+        if (this.riddles.length === 0) {
+          return `PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS "${answer}"???`;
+        } else {
+          return "That wasn't that hard, I bet you don't get the next one";
+        }
       }
+    }
+    if (!this.riddles.some((element) => element.answer === answer)) {
+      this.heroesEaten = this.heroesEaten + 1;
     }
   }
 }

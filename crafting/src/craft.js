7fb0361bd27006d099bcc766db67eaae808d26d2
Katie Marcks <katiemarcks7@gmail.com>
2022-07-13 07:31:40 -0600

    Pass all craft tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,23 @@
 class Craft {
+  constructor({ type, materials }) {
+    this.name = type;
+    this.materials = materials;
+    this.completed = false;
+  }
 
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`;
+  }
+
+  calculateProjectTotal() {
+    let totalCost = 0;
+    for (let i = 0; i < this.materials.length; i++) {
+      const element = this.materials[i];
+      totalCost += element.price * element.amount;
+    }
+    return totalCost;
+  }
 }
 
 module.exports = Craft;

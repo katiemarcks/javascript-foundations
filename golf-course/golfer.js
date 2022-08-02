a914c19b71f6ad1a1f802c2d2abbb661fb692a0e
Katie Marcks <katiemarcks7@gmail.com>
2022-08-02 15:02:45 -0600

    Pass 9 golf course tests

--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,31 @@
 class Golfer {
+  constructor({ name, handicap }) {
+    this.name = name;
+    this.handicap = handicap;
+    this.frustration = 0;
+    this.confidence = 0;
+  }
 
+  calculateAvg(par) {
+    let averageScore = this.handicap + par;
+    return `I usually shoot a ${averageScore} on average.`;
+  }
 
+  playRound(golfCourse) {
+    if (golfCourse.difficulty === "hard") {
+      this.frustration = this.frustration + 500;
+    } else if (golfCourse.difficulty === "moderate") {
+      this.frustration = this.frustration + 100;
+    }
+  }
+
+  hitTheRange() {
+    this.confidence = this.confidence + 10;
+  }
+
+  marvel(golfCourse) {
+    return `I love the ${golfCourse.features.join(" and ")} on this course!`;
+  }
 }
 
 module.exports = Golfer;

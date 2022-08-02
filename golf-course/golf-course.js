a2d65dd3ee55c7364e78ad6b4e7d63043994f73d
Katie Marcks <katiemarcks7@gmail.com>
2022-08-02 15:34:33 -0600

    Pass all golf course tests

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -4,6 +4,21 @@ class GolfCourse {
     this.difficulty = difficulty;
     this.openings = openings;
     this.features = features;
+    this.currentlyPlaying = [];
+  }
+
+  checkInGroup(group) {
+    let groupSize = group.length;
+    if (groupSize <= this.openings) {
+      this.openings = this.openings - groupSize;
+      for (let i = 0; i < group.length; i++) {
+        const golfer = group[i];
+        this.currentlyPlaying.unshift(golfer.name);
+      }
+      return "You're checked in. Have fun!";
+    } else {
+      return "Sorry, we are currently booked! Please come back later.";
+    }
   }
 }
 

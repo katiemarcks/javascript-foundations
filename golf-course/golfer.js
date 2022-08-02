bcd7c2d9ad3f87bec9714e6cb1ad0485d6f109c7
Katie Marcks <katiemarcks7@gmail.com>
2022-08-02 15:18:47 -0600

    Pass 10 golf course tests

--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -26,6 +26,19 @@ class Golfer {
   marvel(golfCourse) {
     return `I love the ${golfCourse.features.join(" and ")} on this course!`;
   }
+
+  whatYaShoot(score) {
+    if (score > 0) {
+      this.frustration = this.frustration + 20;
+      return "Doh!";
+    } else if (score === 0) {
+      this.frustration = this.frustration - 10;
+      return "Booyah!";
+    } else if (score < 0) {
+      this.frustration = 0;
+      return "I AM THE GREATEST GOLFER ALIVE!";
+    }
+  }
 }
 
 module.exports = Golfer;

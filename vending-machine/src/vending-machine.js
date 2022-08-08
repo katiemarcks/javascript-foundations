d5054dfad556cf6cb764872bf6647ae8fdd59ef6
Katie Marcks <katiemarcks7@gmail.com>
2022-08-08 07:46:55 -0600

    Pass 4 vending machine tests

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,17 @@
 class VendingMachine {
+  constructor({ id, isBroken }) {
+    this.id = id;
+    this.isBroken = isBroken;
+    this.snacks = [];
+  }
 
+  addSnacks(snack) {
+    if (!this.snacks.some((e) => e.name === snack["name"])) {
+      this.snacks.push(snack);
+    } else {
+      return "Sorry, that snack is already stocked! Try adding a different snack.";
+    }
+  }
 }
 
 module.exports = VendingMachine;

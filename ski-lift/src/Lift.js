1a17ab83ac3de30597b67b6dd89297f7950f48f9
Katie Marcks <katiemarcks7@gmail.com>
2022-08-10 08:02:10 -0600

    Pass 6 lift tests

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,17 @@
-var Skier = require('./Skier');
+var Skier = require("./Skier");
 
 class Lift {
-  
+  constructor(limit) {
+    this.inService = true;
+    this.limit = limit;
+    this.skiers = [];
+    this.safetyBar = "up";
+  }
+
+  admitSkier(name, hasLiftTicket) {
+    let skier = new Skier(name, hasLiftTicket);
+    this.skiers.push(skier);
+  }
 }
 
 module.exports = Lift;

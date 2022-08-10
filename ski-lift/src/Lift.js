10375d42fadc7d051ade12692b9df401e7da72db
Katie Marcks <katiemarcks7@gmail.com>
2022-08-10 08:15:09 -0600

    Pass 8 lift tests

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -10,8 +10,10 @@ class Lift {
 
   admitSkier(name, hasLiftTicket) {
     let skier = new Skier(name, hasLiftTicket);
-    if (this.skiers.length <= this.limit - 1) {
+    if (this.skiers.length <= this.limit - 1 && skier.hasLiftTicket) {
       this.skiers.push(skier);
+    } else if (!skier.hasLiftTicket) {
+      return `Sorry, ${skier.name}. You need a lift ticket!`;
     } else {
       return `Sorry, ${skier.name}. Please wait for the next lift!`;
     }

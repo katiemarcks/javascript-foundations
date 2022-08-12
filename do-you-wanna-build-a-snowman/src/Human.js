e6e3446e6357fed50b5060a1c578e6d5c8f8bcd5
Katie Marcks <katiemarcks7@gmail.com>
2022-08-12 08:08:08 -0600

    Pass 7 of 8 human tests

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,32 @@
-var Snowman = require('./Snowman');
+var Snowman = require("./Snowman");
 
 class Human {
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      carrots: 0,
+      coal: 0,
+      snowballs: 0,
+      buttons: 0,
+    };
+  }
 
+  gatherMaterials(material, amount) {
+    this.materials[material] = this.materials[material] + amount;
+  }
+
+  buildASnowman() {
+    return new Snowman(this.materials);
+  }
+
+  placeMagicHat() {
+    if (Snowman.magicHat === true) {
+      return `Woah, this snowman is coming to life!`;
+    } else {
+      return `I guess I didn't build it correctly.`;
+    }
+  }
 }
 
 module.exports = Human;

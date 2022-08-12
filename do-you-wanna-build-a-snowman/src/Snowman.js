165032dc7e1e20394de013de806555f949c23fa6
Katie Marcks <katiemarcks7@gmail.com>
2022-08-12 07:33:14 -0600

    Pass 7 snowman tests

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,22 @@
 class Snowman {
+  constructor({ carrots, coal, buttons, snowballs }) {
+    this.carrots = carrots;
+    this.coal = coal;
+    this.buttons = buttons;
+    this.snowballs = snowballs;
+    this.magicHat = false;
+  }
 
+  canWearMagicHat() {
+    if (
+      this.coal >= 2 &&
+      this.buttons >= 5 &&
+      this.carrots >= 1 &&
+      this.snowballs >= 2
+    ) {
+      this.magicHat = true;
+    }
+  }
 }
 
 module.exports = Snowman;

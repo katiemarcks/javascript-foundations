4b6d2332de48f60b6f1e643a3c70877325fbb35c
Katie Marcks <katiemarcks7@gmail.com>
2022-08-19 06:28:33 -0600

    Pass 12 ship tests

---
+++ spaceport/src/ship.js
@@ -0,0 +1,28 @@
+class Ship {
+  constructor({
+    name,
+    type,
+    maxCrew,
+    odometer = 0,
+    fuelCapacity = 10,
+    captain,
+  }) {
+    this.name = name;
+    this.type = type;
+    this.maxCrew = maxCrew;
+    this.odometer = odometer;
+    this.fuelCapacity = fuelCapacity;
+    this.fuel = 0;
+    this.captain = captain;
+    this.crew = [];
+  }
+
+  addCrew(crew) {
+    for (let i = 0; i < crew.length; i++) {
+      const element = crew[i];
+      this.crew.push(element);
+    }
+  }
+}
+
+module.exports = Ship;

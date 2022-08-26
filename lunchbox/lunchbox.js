52bae6948f3a93ae9fe36072c2fb970e7905ac5b
Katie Marcks <katiemarcks7@gmail.com>
2022-08-26 06:39:37 -0600

    Pass all lunchbox tests

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,27 @@
 class LunchBox {
+  constructor({ owner, madeOf, shape, color }) {
+    this.owner = owner;
+    this.material = madeOf;
+    this.shape = shape;
+    this.color = color;
+    this.snacks = [];
+  }
 
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    this.healthySnacks = [];
+    for (let i = 0; i < this.snacks.length; i++) {
+      const element = this.snacks[i];
+      if (element.type.toLowerCase().includes("fruit")) {
+        this.healthySnacks.push(element.type);
+      }
+    }
+    return this.healthySnacks;
+  }
 }
 
 module.exports = LunchBox;

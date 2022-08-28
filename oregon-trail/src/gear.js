e7c4f3680df208ef8a9712c0ba60a3af0f9c5bb1
Katie Marcks <katiemarcks7@gmail.com>
2022-08-28 12:37:01 -0600

    Pass all gear tests

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,22 @@
 class Gear {
+  constructor(type, quantity) {
+    this.type = type;
+    this.quantity = quantity;
+  }
 
+  checkForValidity() {
+    if (
+      this.type === "ammunition" ||
+      this.type === "clothes" ||
+      this.type === "food"
+    ) {
+      return `Great, we'll need lots of ${this.type}!`;
+    } else {
+      let invalidMessage = `I don't think a ${this.type} will help us.`;
+      this.type = null;
+      return invalidMessage;
+    }
+  }
 }
 
 module.exports = Gear;

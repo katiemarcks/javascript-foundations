0402336b1367b1a8afad98abad838f659f06fdb9
Katie Marcks <katiemarcks7@gmail.com>
2022-08-30 05:27:59 -0600

    Pass all part tests

--- oregon-trail/src/part.js
+++ oregon-trail/src/part.js
@@ -1,5 +1,22 @@
 class Part {
+  constructor(type) {
+    this.type = type;
+    this.broken = false;
+    this.breakCount = 0;
+  }
 
+  break() {
+    this.breakCount = this.breakCount + 1;
+    this.broken = true;
+  }
+
+  repair() {
+    if (this.breakCount >= 2) {
+      return `This ${this.type} has seen better days, we'll need a brand new one!`;
+    } else {
+      this.broken = false;
+    }
+  }
 }
 
 module.exports = Part;
--- oregon-trail/test/part-test.js
+++ oregon-trail/test/part-test.js
@@ -1,26 +1,26 @@
-var assert = require('chai').assert;
-var Part = require('../src/part.js');
+var assert = require("chai").assert;
+var Part = require("../src/part.js");
 
-describe('Part', function() {
-  it.skip('should have a type', function() {
-    var wheel = new Part('wheel');
-    var yoke = new Part('yoke');
+describe("Part", function () {
+  it("should have a type", function () {
+    var wheel = new Part("wheel");
+    var yoke = new Part("yoke");
 
-    assert.equal(wheel.type, 'wheel');
-    assert.equal(yoke.type, 'yoke');
+    assert.equal(wheel.type, "wheel");
+    assert.equal(yoke.type, "yoke");
   });
 
-  it.skip('should not be broken by default', function() {
-    var wheel = new Part('wheel');
-    var yoke = new Part('yoke');
+  it("should not be broken by default", function () {
+    var wheel = new Part("wheel");
+    var yoke = new Part("yoke");
 
     assert.equal(wheel.broken, false);
     assert.equal(yoke.broken, false);
   });
 
-  it.skip('should be able to be broken', function() {
-    var wheel = new Part('wheel');
-    var yoke = new Part('yoke');
+  it("should be able to be broken", function () {
+    var wheel = new Part("wheel");
+    var yoke = new Part("yoke");
 
     wheel.break();
     yoke.break();
@@ -29,8 +29,8 @@ describe('Part', function() {
     assert.equal(wheel.broken, true);
   });
 
-  it.skip('should be able to be repaired', function() {
-    var yoke = new Part('yoke');
+  it("should be able to be repaired", function () {
+    var yoke = new Part("yoke");
 
     yoke.break();
     yoke.repair();
@@ -38,9 +38,9 @@ describe('Part', function() {
     assert.equal(yoke.broken, false);
   });
 
-  it.skip('should not be able to be repaired if it is broken a second time', function() {
-    var yoke = new Part('yoke');
-    var wheel = new Part('wheel');
+  it("should not be able to be repaired if it is broken a second time", function () {
+    var yoke = new Part("yoke");
+    var wheel = new Part("wheel");
 
     yoke.break();
     wheel.break();
@@ -60,8 +60,14 @@ describe('Part', function() {
     wheel.repair();
 
     assert.equal(yoke.broken, true);
-    assert.equal(yoke.repair(), 'This yoke has seen better days, we\'ll need a brand new one!');
+    assert.equal(
+      yoke.repair(),
+      "This yoke has seen better days, we'll need a brand new one!"
+    );
     assert.equal(wheel.broken, true);
-    assert.equal(wheel.repair(), 'This wheel has seen better days, we\'ll need a brand new one!');
+    assert.equal(
+      wheel.repair(),
+      "This wheel has seen better days, we'll need a brand new one!"
+    );
   });
-})
+});
